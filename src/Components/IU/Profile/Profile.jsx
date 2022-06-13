05a4ce16cd508f4de42487f1333e5a97f28b5d57
sant <santer28gamer@gmail.com>
2022-06-13 15:07:16 -0500

    edited user profile view
    * entries are blocked with not-allowed
    
    * the waiting skeleton was made

--- src/Components/IU/Profile/Profile.jsx
+++ src/Components/IU/Profile/Profile.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Profile.css'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 export const Profile = ({ user }) => {
   return (
@@ -7,10 +9,38 @@ export const Profile = ({ user }) => {
         <div className="containerImgProfile">
             <img src={user.foto} alt={user.nombre} className="imgProfile"/>
         </div>
-        <h1>Documento: </h1> <p>{user.documento}</p>
-        <h1>Nombre: </h1> <p>{user.nombre}</p>
-        <h1>Apellido: </h1> <p>{user.apellido}</p>
-        <h1>Fecha: </h1> <p>{user.fecnac}</p>
+        <div className='ContInfor'>
+
+
+
+
+
+    <Box
+
+    >
+    <div className='ContImput'>
+      <div className='cursor'>
+        <TextField className='TextField'  not-allowed id="demo-helper-text-misaligned-no-helper"   value={user.documento} />
+      </div>
+      <div className='cursor1'>
+        <TextField className='TextField'  id="demo-helper-text-misaligned-no-helper"  value={user.nombre} />
+
+      </div>
+      <div className='cursor2'>
+        <TextField className='TextField'  id="demo-helper-text-misaligned-no-helper"  value={user.apellido} />
+
+      </div>
+      <div className='cursor3'>
+        <TextField className='TextField'  id="demo-helper-text-misaligned-no-helper"  value={user.fecnac}/>
+
+      </div>
+      
+
+      <button className='Actualizar' >Actualizar</button>
+    </div>
+    </Box>
+    <input className='prueba2' type="text" mozactionhint="next"></input>
+        </div>
     </div>
   )
 }
