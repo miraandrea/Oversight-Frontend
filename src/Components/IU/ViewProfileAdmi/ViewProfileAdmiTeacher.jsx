7ee063c336883469c0bf572396e105c087eaf4ac
githubandrea123 <miraandrea>
2022-06-21 00:21:11 -0500

    the administrator's and teacher's styles have been completed.
    *administrator: we refactored the student annotation history menu and the profile.
    
    *teacher: refactored the menu, added an annotation and the profile. the teacher's functionality was completed.

--- src/Components/IU/ViewProfileAdmi/ViewProfileAdmiTeacher.jsx
+++ src/Components/IU/ViewProfileAdmi/ViewProfileAdmiTeacher.jsx
@@ -1,31 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import './ViewProfileAdmi.css'
 import axios from "axios";
-import jwtDecode from "jwt-decode";
+import './ViewProfileAdmi.css'
 import { useParams } from "react-router";
+import React, { useEffect, useState } from 'react'
 
 //components
-import { CardStudent } from '../CardStudent/CardStudent';
 import foto from '../ImgProfile/student.jpg'
+import { CardStudent } from '../CardStudent/CardStudent';
 
-//material ui
+//librerias
+import jwtDecode from "jwt-decode";
 import { AiOutlineFileSearch } from "react-icons/ai";
-
 
 export const ViewProfileAdmiTeacher = ({ teacher }) => {
 
   //historial 
-
-  console.log(teacher);
-
   const { name } = useParams()
   const [record, setRecord] = useState([]);
 
-  const UrlTokenRecord = "http://localhost:4000/v1/teachers/" + name + "/observers";
-
   useEffect(() => {
     const getRecord = () => {
-      axios.get(UrlTokenRecord)
+      axios.get(`http://localhost:4000/v1/teachers/${name}/observers`)
         .then((res) => {
           const token = jwtDecode(res.data)
           setRecord(token.results)
@@ -39,41 +33,42 @@ export const ViewProfileAdmiTeacher = ({ teacher }) => {
     <div>
       <img src={teacher.fotoDocente || foto} alt="photo" className="photoView" />
       <div className='centerInfor'>
-      <div className='information'>
-        <p>{teacher.nombreDocente} {teacher.apellidoDocente}</p>
-        <p>{teacher.documentoDocente }</p>
-        <p>Docente</p>
-      </div>
+        <div className='information'>
+          <p>{teacher.nombreDocente} {teacher.apellidoDocente}</p>
+          <p>{teacher.documentoDocente}</p>
+          <p>Docente</p>
+        </div>
       </div>
       <div className="iconRecord">
         <AiOutlineFileSearch />
         <p>Historial</p>
       </div>
       <div className='centerOver'>
-      <div className='OverFlow'>
-      <div className="cards1" >
-        <div className="cardInfo1">
-          {record.length > 0 ? (
-            record.map((course2, index) => (
-              <CardStudent key={index} course2={course2}/>
-            ))
-          ): (
-            <div className='mensaje2' >
-            <p>No tiene anotaciones</p>
+        <div className='OverFlow'>
+          <div className="cards1" >
+            <div className="cardInfo1">
+              {record.length > 0 ? (
+                record.map((course2, index) => (
+                  <CardStudent key={index} course2={course2} />
+                ))
+              ) : (
+                <div className='mensaje2' >
+                  <p className='mensaje' >No tiene anotaciones</p>
+
+                </div>
+              )}
             </div>
-          )}
+          </div>
         </div>
       </div>
-      </div>
-      </div>
       <div className='prueba'>
-      <div className='centerBtn' >
-      <div className="btn_Cancel1">
-        <button className="update">Actualizar</button>
-        <button className="disable">Deshabilitar</button>
+        <div className='centerBtn' >
+          <div className="btn_Cancel1">
+            <button className="update">Actualizar</button>
+            <button className="disable">Deshabilitar</button>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
     </div>
   )
-}
+};
